2087bce465b2b6221bdaccfaaf457b9c921fab3a
NickPapayiannakis <nikos.papayiannakis@gmail.com>
2017-02-02 09:37:20 -0500

    Now returns combo of two longest strings, though not in order

--- longestConsec/src/StringArr.js
+++ longestConsec/src/StringArr.js
@@ -1,9 +1,4 @@
 var longestConsec = function(strarr, k){
-	//expect(longestConsec(["a", "bb","cc"],2)).toEqual("bbcc"); TODO
-
 	if (k > strarr.length || k <= 0) {return ""}
-
-	return strarr.reduce(function(prev, curr){
-		return prev.length > curr.length? prev : curr;
-	}, "");
+	return strarr.sort(function(a, b) {return a.length - b.length;}).slice(strarr.length - k, strarr.length).join("");
 }
